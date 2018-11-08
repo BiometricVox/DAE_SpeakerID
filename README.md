232692f93ca24b4a88f6c40ab73b7072d30f998c
RobertoFont <robertojavier.font@gmail.com>
2018-11-08 13:07:01 +0100

    Readme and DAE approach

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# MCE2018
+# A DENOISING AUTOENCODER FOR SPEAKER IDENTIFICATION
+
+This is a Python implementation of the Denoising Autoencoder approach that we proposed for the first Multi-target speaker detection and identification Challenge Evaluation (MCE 2018, [http://www.mce2018.org](http://www.mce2018.org) ).
+
+The basic idea is to train a Denoising Autoencoder to map each individual input ivector to the mean of all ivectors from that speaker. The aim of this DAE is to compensate for inter-session variability and increase the discriminative power of the ivectors.
+
+You can find our system description for the MCE 2018 challenge [here](http://mce.csail.mit.edu/pdfs/BiometricVox_description.pdf).
+
+## ABOUT THE MCE 2018 CHALLENGE
+
+The task for the MCE 2018 Evaluation was to detect if a given speech segment belongs to any of the speakers in a blacklist. The challenge is divided into two related subtasks: Top-S detection, i.e. detecting if the segment belongs to any of the blacklist speakers; and Top-1 detection, i.e. detecting which specific blacklist speaker (if any) is speaking in the segment. The data was generated from real call center user-agent telephone conversations. Instead of raw audio data, organizers processed the original data and provided 600-dimensional ivectors. This way, no special signal processing knowledge was needed to enter the evaluation. More details can be found on the [evaluation plan](https://arxiv.org/abs/1807.06663).
+
+## DATASET
+
+The dataset can be found at:
+
+[https://www.kaggle.com/kagglesre/blacklist-speakers-dataset](https://www.kaggle.com/kagglesre/blacklist-speakers-dataset)
+
+After download, extract the files to _data_ folder.
+
+## SYSTEM TRAINING
+
+Our training script shows how a very simple DAE can bring a very nice improvement over the [baseline](https://github.com/swshon/multi-speakerID). If you run
+
+```
+python mce2018_dae_tst.py
+```
+
+you should get results like these:
+
+```
+Dev set score using train set :
+Top S detector EER is 2.40%
+Top 1 detector EER is 9.50% (Total confusion error is 343)
+
+Test set score using train set:
+Top S detector EER is 6.83%
+Top 1 detector EER is 12.42% (Total confusion error is 411)
+
+Test set score using train + dev set:
+Top S detector EER is 5.69%
+Top 1 detector EER is 8.90% (Total confusion error is 257)
+```
+
+Note that these results do not match our official submission to the challenge, were we obtained Top-S EER: 4.33%, Top-1 EER: 6.11%, since our final system was a bit more complex including Probabilistic Linear Discriminant Analysis (PLDA) scoring and Symmetric Normalization (S-Norm).
+
+
--- mce2018_dae_tst.py
+++ mce2018_dae_tst.py
@@ -2,8 +2,24 @@ import numpy as np
 
 from mce_utils import load_ivector, length_norm, make_spkvec, calculate_EER, get_trials_label_with_confusion, calculate_EER_with_confusion
 
-## making dictionary to find blacklist pair between train and test dataset
-# bl_match = np.loadtxt('data/bl_matching_dev.csv',dtype='string')
+import pandas as pd
+from keras.models import Model
+from keras.layers import Input, Dense, Activation
+from keras import metrics
+from keras import optimizers
+
+def get_DAE(nu=2000):
+  iv_dim = 600
+  inputs = Input(shape=(iv_dim,))
+  x = Dense(nu)(inputs)
+  x = Activation('tanh')(x)
+  x = Dense(iv_dim)(x)
+  out = Activation('linear')(x)
+  model = Model(inputs=inputs, outputs=out)
+  
+  return model
+
+# Making dictionary to find blacklist pair between train and test dataset
 bl_match = np.loadtxt('data/bl_matching.csv',dtype='string')
 dev2train={}
 dev2id={}
@@ -20,25 +36,7 @@ for iter, line in enumerate(bl_match):
     test2train[line_s[2].split('_')[-1]]= line_s[3].split('_')[-1]
     train2test[line_s[3].split('_')[-1]]= line_s[2].split('_')[-1]
     
-
-
-# Loading i-vector
-trn_bl_id, trn_bl_utt, trn_bl_ivector = load_ivector('data/trn_blacklist.csv')
-trn_bg_id, trn_bg_utt, trn_bg_ivector = load_ivector('data/trn_background.csv')
-dev_bl_id, dev_bl_utt, dev_bl_ivector = load_ivector('data/dev_blacklist.csv')
-dev_bg_id, dev_bg_utt, dev_bg_ivector = load_ivector('data/dev_background.csv')
-tst_id, test_utt, tst_ivector = load_ivector('data/tst_evaluation.csv')
-
-# Calculating speaker mean vector
-spk_mean, spk_mean_label = make_spkvec(trn_bl_ivector,trn_bl_id)
-
-#length normalization
-trn_bl_ivector = length_norm(trn_bl_ivector)
-trn_bg_ivector = length_norm(trn_bg_ivector)
-dev_bl_ivector = length_norm(dev_bl_ivector)
-dev_bg_ivector = length_norm(dev_bg_ivector)
-tst_ivector = length_norm(tst_ivector)
-
+    
 # load test set information
 filename = 'data/tst_evaluation_keys.csv'
 tst_info = np.loadtxt(filename,dtype='string',delimiter=',',skiprows=1,usecols=range(0,3))
@@ -52,22 +50,72 @@ for iter in range(len(tst_info)):
         
     else:
         tst_trials = np.append(tst_trials,1)
+    
 
+# Fix random seed to make results reproducible
+seed = 134
+np.random.seed(seed)
+
+# Loading i-vector
+trn_bl_id, trn_bl_utt, trn_bl_ivector = load_ivector('data/trn_blacklist.csv')
+trn_bg_id, trn_bg_utt, trn_bg_ivector = load_ivector('data/trn_background.csv')
+dev_bl_id, dev_bl_utt, dev_bl_ivector = load_ivector('data/dev_blacklist.csv')
+dev_bg_id, dev_bg_utt, dev_bg_ivector = load_ivector('data/dev_background.csv')
+tst_id, test_utt, tst_ivector = load_ivector('data/tst_evaluation.csv')
+
+# length normalization
+trn_bl_ivector = length_norm(trn_bl_ivector)
+trn_bg_ivector = length_norm(trn_bg_ivector)
+dev_bl_ivector = length_norm(dev_bl_ivector)
+dev_bg_ivector = length_norm(dev_bg_ivector)
+tst_ivector = length_norm(tst_ivector)
+
+# Inputs to DAE are ivectors and targets are the speaker-level mean ivectors
+train_spk_ids = pd.DataFrame({'spk_ids': trn_bg_id})
+train_ivs = pd.DataFrame(trn_bg_ivector)
+
+X_train = train_ivs.values
+Y_train = (train_ivs.groupby(train_spk_ids['spk_ids']).transform('mean')).as_matrix()
+
+# DAE training
+model = get_DAE()
+
+model.compile(loss='cosine_proximity',
+              optimizer = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=1e-06),
+              metrics=[metrics.mean_squared_error])
+
+num_examples = X_train.shape[0]
+num_epochs = 5
+batch_size = 512
+num_batch_per_epoch = num_examples / batch_size
+
+model.fit(x=X_train,y=Y_train,batch_size=batch_size,epochs=num_epochs)
+
+# Compute DAE-transformed embeddings from ivectors
+trn_bl_embeddings = model.predict(trn_bl_ivector,batch_size=batch_size)
+trn_bg_embeddings = model.predict(trn_bg_ivector,batch_size=batch_size)
+dev_bl_embeddings = model.predict(dev_bl_ivector,batch_size=batch_size)
+dev_bg_embeddings = model.predict(dev_bg_ivector,batch_size=batch_size)
+tst_embeddings = model.predict(tst_ivector,batch_size=batch_size)
+
+# Calculating speaker mean vector
+spk_mean, spk_mean_label = make_spkvec(trn_bl_embeddings,trn_bl_id)
+
+# length normalization
+trn_bl_embeddings = length_norm(trn_bl_embeddings)
+trn_bg_embeddings = length_norm(trn_bg_embeddings)
+dev_bl_embeddings = length_norm(dev_bl_embeddings)
+dev_bg_embeddings = length_norm(dev_bg_embeddings)
+tst_embeddings = length_norm(tst_embeddings)
         
 print '\nDev set score using train set :'
+
 # making trials of Dev set
-dev_ivector = np.append(dev_bl_ivector, dev_bg_ivector,axis=0)
+dev_embeddings = np.append(dev_bl_embeddings, dev_bg_embeddings,axis=0)
 dev_trials = np.append( np.ones([len(dev_bl_id), 1]), np.zeros([len(dev_bg_id), 1]))
 
 # Cosine distance scoring
-scores = spk_mean.dot(dev_ivector.transpose())
-
-# Multi-target normalization
-blscores = spk_mean.dot(trn_bl_ivector.transpose())
-mnorm_mu = np.mean(blscores,axis=1)
-mnorm_std = np.std(blscores,axis=1)
-for iter in range(np.shape(scores)[1]):
-    scores[:,iter]= (scores[:,iter] - mnorm_mu) / mnorm_std
+scores = spk_mean.dot(dev_embeddings.transpose())
 dev_scores = np.max(scores,axis=0)
 
 # Top-S detector EER
@@ -82,15 +130,9 @@ dev_trials_confusion = get_trials_label_with_confusion(dev_identified_label, dev
 dev_EER_confusion = calculate_EER_with_confusion(dev_scores,dev_trials_confusion)
 
 print '\nTest set score using train set:'
-#Cosine distance scoring on Test set
-scores = spk_mean.dot(tst_ivector.transpose())
 
-# Multi-target normalization
-blscores = spk_mean.dot(trn_bl_ivector.transpose())
-mnorm_mu = np.mean(blscores,axis=1)
-mnorm_std = np.std(blscores,axis=1)
-for iter in range(np.shape(scores)[1]):
-    scores[:,iter]= (scores[:,iter] - mnorm_mu) / mnorm_std
+#Cosine distance scoring on Test set
+scores = spk_mean.dot(tst_embeddings.transpose())
 tst_scores = np.max(scores,axis=0)
 
 # top-S detector EER
@@ -105,25 +147,17 @@ tst_EER_confusion = calculate_EER_with_confusion(tst_scores,tst_trials_confusion
 
 
 print '\nTest set score using train + dev set:'
+
 # get dev set id consistent with Train set
 dev_bl_id_along_trnset = []
 for iter in range(len(dev_bl_id)):
     dev_bl_id_along_trnset.extend([dev2train[dev_bl_id[iter]]])
 
 # Calculating speaker mean vector
-spk_mean, spk_mean_label = make_spkvec(np.append(trn_bl_ivector,dev_bl_ivector,0),np.append(trn_bl_id,dev_bl_id_along_trnset))
+spk_mean, spk_mean_label = make_spkvec(np.append(trn_bl_embeddings,dev_bl_embeddings,0),np.append(trn_bl_id,dev_bl_id_along_trnset))
 
 #Cosine distance scoring on Test set
-scores = spk_mean.dot(tst_ivector.transpose())
-# tst_scores = np.max(scores,axis=0)
-
-
-# Multi-target normalization
-blscores = spk_mean.dot(np.append(trn_bl_ivector.transpose(),dev_bl_ivector.transpose(),axis=1))
-mnorm_mu = np.mean(blscores,axis=1)
-mnorm_std = np.std(blscores,axis=1)
-for iter in range(np.shape(scores)[1]):
-    scores[:,iter]= (scores[:,iter] - mnorm_mu) / mnorm_std
+scores = spk_mean.dot(tst_embeddings.transpose())
 tst_scores = np.max(scores,axis=0)
 
 # top-S detector EER
